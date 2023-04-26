c5c05db0761c81bf94825b9017aa6aa658ea64e1
chrisupt2001 <73676459+chrisupt2001@users.noreply.github.com>
2023-04-26 16:36:14 -0400

    v1.2

--- components/NonSocialLinks.jsx
+++ components/NonSocialLinks.jsx
@@ -1,7 +1,39 @@
-import { getnonSocialLinks } from "csc-start/utils/data";
+"use client"
+import {getLinksLinks}from "csc-start/utils/data";
+import { useEffect, useState } from 'react';
 
 const NonSocialLinks = () => {
-    return <div className="barge"><p>Non Social Links</p></div>
+    
+    const [nonSocLinks, setNonSocLinks] = useState([]);
+
+    useEffect(() => {
+
+        setNonSocLinks(getLinksLinks());
+        console.log(getLinksLinks());
+
+    },[]);
+    
+    return( 
+    
+    <div className="barge">
+      <div>
+        <button className='button'>LINK 1</button>
+      </div>
+      <div>
+        <button className='button'>LINK 2</button>
+      </div>
+      <div>
+        <button className='button'>LINK 3</button>
+      </div>
+      <div>
+        <button className='button' link to='https://google.com'>
+          LINK 4
+        </button>
+      </div>
+    </div>
+
+    
+    )
 }
 
 export default NonSocialLinks;
